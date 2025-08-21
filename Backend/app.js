2f8de8a4c80c72daa801bcea85771c81dff9fd25
Revanasidda Nimbal <revanasiddanimbal82@gmail.com>
2025-08-22 01:13:53 +0530

    linked express.json

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen("1299", () => {
   console.log("server started");
 });
