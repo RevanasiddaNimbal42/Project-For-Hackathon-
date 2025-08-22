3a58f96c0caf88443fbf51de6edd8790a5d6b577
Revanasidda Nimbal <revanasiddanimbal82@gmail.com>
2025-08-23 01:46:21 +0530

    removed unecessaries

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,8 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
-// Load environment variables
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-// Import MongoDB connection
 const connectDB = require("./config/db");
 
 // Connect to MongoDB
@@ -16,21 +14,19 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // To parse JSON requests
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve uploaded images
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
-app.use("/api/auth", require("./routes/authRouter"));      // Auth routes (already existing)
-app.use("/api/artworks", require("./routes/artworkRoutes")); // ✅ Added Artwork routes 
+app.use("/api/auth", require("./routes/authRouter"));
+app.use("/api/artworks", require("./routes/artworkRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
-
 
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
