998b906bb1a7c96b73fc7dafe7f4565a70323d41
Revanasidda Nimbal <revanasiddanimbal82@gmail.com>
2025-08-22 19:42:08 +0530

    added authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,28 @@
 const dotenv = require("dotenv");
 const path = require("path");
+const express = require("express");
+const cors = require("cors");
 
+// Load environment variables
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-const express = require("express");
+// Import MongoDB connection
+const connectDB = require("./config/db");
+
+// Connect to MongoDB
+connectDB();
+
 const app = express();
 
-//Require port
+// Middleware
+app.use(cors());
+app.use(express.json()); // To parse JSON requests
+
+// Routes
+app.use("/api/auth", require("./routes/authRouter"));
+
+// Start server
 const port = process.env.PORT || 3000;
-// console.log("Port: ", process.env.PORT);
-
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
-
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port} `);
+  console.log(`Server started at http://localhost:${port}`);
 });
